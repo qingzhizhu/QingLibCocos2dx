a1b94eb25a331078664128b616a427e0a200c67f
耿坤 <gengkun@elex-tech.com>
2014-07-25 16:04:11 +0800

    AssetsManager 下载测试 1. 不同包内的相同名称会覆盖旧版的。 2. 需找图片路径优先查找下载目录的资源， 3. 下载目录放在第一个位置，下载成功，会没有新版本下载会自动调用AssetsManager.setSearchPath 设置路径。

--- QingLibCocos2dx/Classes/demo/AssetsManager/MyAssetsManagerDemo.cpp
+++ QingLibCocos2dx/Classes/demo/AssetsManager/MyAssetsManagerDemo.cpp
@@ -15,8 +15,8 @@ USING_NS_QING;
 static MyAssetsManager* manager;
 MyAssetsManagerDemo::MyAssetsManagerDemo()
 {
-    manager = new MyAssetsManager("http://10.1.21.16:8099/demo/package.zip", "http://10.1.21.16:8099/demo/version", this, callfunc_selector(MyAssetsManagerDemo::callBack));
-    manager->removeDownload();        //若主动删除文件内容，version信息不会更新
+    manager = new MyAssetsManager("http://localhost/demo/package.zip", "http://localhost/demo/version", this, callfunc_selector(MyAssetsManagerDemo::callBack));
+//    manager->removeDownload();        //若主动删除文件内容，version信息不会更新
     manager->startDownload();
 }
 
@@ -29,15 +29,9 @@ void MyAssetsManagerDemo::callBack()
 {
     CCLOG("更新完毕！");
     if(!manager->getIsSuccessed()) return;
-    CCSprite* img = CCSprite::create("I_9.png");
-    img->cocos2d::CCNode::setPosition(200, 300);
-    addChild(img);
-    img = CCSprite::create("I_06.png");
-    img->cocos2d::CCNode::setPosition(100, 100);
-    addChild(img);
     
-    img = CCSprite::create("I_15.png");
-    img->cocos2d::CCNode::setPosition(200, 200);
+    CCSprite* img = CCSprite::create("1_s.png");
+    img->cocos2d::CCNode::setPosition(200, 300);
     addChild(img);
     
 }
